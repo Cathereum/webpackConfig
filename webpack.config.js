2ac653802a318a86ea7cb3faec6b65fc305de6ac
Cathereum <106606021+Cathereum@users.noreply.github.com>
2023-12-13 16:18:39 -0300

    npm install --save-dev typescript ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = (env) => {
   return {
     mode: env.mode ?? "development",
-    entry: path.resolve(__dirname, "src", "index.js"),
+    entry: path.resolve(__dirname, "src", "index.ts"),
     output: {
       path: path.resolve(__dirname, "build"),
       filename: "[name].[contenthash].js",

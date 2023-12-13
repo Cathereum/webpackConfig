0ea459c26126e1ee829e11ec7efa36c955b30c2e
Cathereum <106606021+Cathereum@users.noreply.github.com>
2023-12-13 14:45:14 -0300

    сделали аутпут куда  билдить проект и название

--- webpack.config.js
+++ webpack.config.js
@@ -2,4 +2,8 @@ const path = require("path");
 
 module.exports = {
   entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
 };
